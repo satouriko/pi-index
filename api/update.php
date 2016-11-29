929106a1ff2f2667ce1a6e0078157037a1917f57
root <root@TENCENT.site>
2016-11-29 15:32:37 +0800

    bugfix

--- api/update.php
+++ api/update.php
@@ -11,11 +11,11 @@
 $newip = $_REQUEST["ip"];
 $newipv6 = $_REQUEST["ipv6"];
 $pwd = $_REQUEST["pwd"];
-if ($newip != "" && $pwd == "raspberry") {
+if (isset($_REQUEST["ip"]) && $pwd == "raspberry") {
     require_once("Db.php");
     $Db = new DB_MYSQL();
     $newips = explode(' ', $newip);
-    if($newipv6 == "" && count($newips) > 1)
+    if(!isset($_REQUEST["ipv6"]) && count($newips) > 1)
         $newipv6 = $newips[1];
     $ip = array(
         "ip" => $newip,
